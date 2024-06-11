ec1f2fa159eb2f1a45c1797d7c4cbaf3aa5ccff7
Hunny <unreal5_gonza@outlook.es>
2024-06-11 18:10:39 -0300

    Agrega cambios

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
-function mostrarPorfileButtons() {
-    document.getElementById('buttons-header').style.display = 'none';
-    document.getElementById('profile-header').style.display = 'flex';
+function mostrarPorfileButtonsPostRegister() {
+    const email = document.getElementById('email-register').value
+
+    switch (email) {
+        case "usuario@gmail.com":
+            document.getElementById('buttons-header').style.display = 'none';
+            document.getElementById('profile-header').style.display = 'flex';
+            break
+        case "administrador@gmail.com":
+            window.location.href = 'administrador.html';
+            break
+        case "marketing@gmail.com":
+            window.location.href = 'marketing.html';
+            break
+        case "controldesalas@gmail.com":
+            window.location.href = 'empleado.html';
+            break
+        case "vendedorporventanilla@gmail.com":
+            window.location.href = 'vendedor.html';
+            break
+        default:
+            break
+    }
+}
+
+function mostrarPorfileButtonsPostLogin() {
+    const email = document.getElementById('email').value
+
+    switch (email) {
+        case "usuario@gmail.com":
+            document.getElementById('buttons-header').style.display = 'none';
+            document.getElementById('profile-header').style.display = 'flex';
+            break
+        case "administrador@gmail.com":
+            window.location.href = 'administrador.html';
+            break
+        case "marketing@gmail.com":
+            window.location.href = 'marketing.html';
+            break
+        case "controldesalas@gmail.com":
+            window.location.href = 'empleado.html';
+            break
+        case "vendedorporventanilla@gmail.com":
+            window.location.href = 'vendedor.html';
+            break
+        default:
+            break
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
